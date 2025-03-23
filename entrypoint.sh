82889de7a75ae093b4b532303d48a90fbcb7e278
VV-Manoj <vardhinenimanoj4@gmail.com>
2025-03-23 15:53:45 +0530

    entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Define the URL of the new script
+rm -rf  /install.sh
+SCRIPT_URL="https://raw.githubusercontent.com/astuto-ai/onelens-installation-scripts/refs/heads/master/install.sh"
+
+# Download the new script
+curl -H 'Cache-Control: no-cache' -fsSL "$SCRIPT_URL" -o /install.sh
+
+# Make the script executable
+chmod +x /install.sh
+# Run the new script
+exec /install.sh 
+
